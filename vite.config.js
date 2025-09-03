02ed53ab9e1039d4bc57844a6676d84430241012
VinSnow <u1609175669@gmail.com>
2025-09-03 15:08:09 +0200

    Add rollup fix for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',   // directory di output
+    outDir: 'dist',
   },
   server: {
-    port: 5173,       // porta locale
-    open: true        // apre il browser automaticamente
-  }
+    port: 5173,
+    open: true,
+  },
 })
